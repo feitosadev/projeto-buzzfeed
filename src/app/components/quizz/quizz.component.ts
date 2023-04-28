94f99e4d484c0ef0fe60e80237bae8d92ff4495e
Jose Carlos Feitosa <92383795+feitosadev@users.noreply.github.com>
2023-04-28 03:39:01 -0300

    Add files via upload
    Finalizando mais um projeto da DIO com ANgular.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -7,25 +7,63 @@ import quizz_questions from "../../../assets/data/quizz_questions.json";
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent {
-  title:string = ""
+  title:string = "";
 
-  questions:any
-  questionSelected:any
+  questions:any;
+  questionSelected:any;
 
-  answers:string[] = []
-  answerSelected:string = ""
+  answers:string[] = [];
+  answerSelected:string = "";
 
-  questionIndex:number = 0
-  questionIndexMax:number = 0
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
 
-  finished:boolean = false
+  finished:boolean = false;
 
   ngOnInit(): void{
     if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
     }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value);
+    this.nextStep()
+    console.log(this.answers)
+  }
+
+  async nextStep(){
+    this.questionIndex += 1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    }
+    else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+      console.log(this.answers)
+    }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length){
+        return previous
+      }
+      else{
+        return current
+      }
+    })
+    return result
   }
 }
